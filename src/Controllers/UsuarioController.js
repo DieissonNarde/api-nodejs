0ac016c9a83ef257cf64eed1493e7c08b54f30a6
DieissonNarde <dieissonnarde@gmail.com>
2022-06-26 09:58:53 -0400

    first commit

---
+++ src/Controllers/UsuarioController.js
@@ -0,0 +1,31 @@
+exports.post = (req, res, next) => {
+  res.status(201).send('Rota POST!')
+}
+
+exports.put = (req, res, next) => {
+  let id = req.params.id
+  res.status(201).send(`Rota PUT com ID! --> ${id}`)
+}
+
+exports.delete = (req, res, next) => {
+  let id = req.params.id
+  res.status(200).send(`Rota DELETE com ID! --> ${id}`)
+}
+
+exports.get = (req, res, next) => {
+  res.status(200).send('Rota GET!')
+}
+
+exports.getById = (req, res, next) => {
+  let id = req.params.id
+  res.status(200).send(`Rota GET com ID! ${id}`)
+}
+
+const usuarioLogado = false
+
+exports.use = (req, res, next) => {
+  if (!usuarioLogado) {
+    res.redirect('/')
+  }
+  next() //middleware
+}
